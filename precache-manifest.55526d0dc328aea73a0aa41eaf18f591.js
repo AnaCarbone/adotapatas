f252c849f4f22193c453ee7fe8f047f6a660c7fc
AnaCarbone <anabcarbone@gmail.com>
2019-11-17 09:45:10 -0300

    Updates

---
+++ precache-manifest.55526d0dc328aea73a0aa41eaf18f591.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893cafcde71b21abfa9f1b29747d7b9a",
+    "url": "/adotapatas/index.html"
+  },
+  {
+    "revision": "dfabfead7e98461a7ce8",
+    "url": "/adotapatas/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2d0e9f477688f04284be",
+    "url": "/adotapatas/static/js/2.c1ba12b7.chunk.js"
+  },
+  {
+    "revision": "dfabfead7e98461a7ce8",
+    "url": "/adotapatas/static/js/main.397ea631.chunk.js"
+  },
+  {
+    "revision": "b6f8621778ca3736ff43",
+    "url": "/adotapatas/static/js/runtime-main.02616eac.js"
+  },
+  {
+    "revision": "359a652920baef71280c7f279cf4694b",
+    "url": "/adotapatas/static/media/cat-miguel.359a6529.jpg"
+  },
+  {
+    "revision": "ce017ccec792aef89c34f0cb20c4e97d",
+    "url": "/adotapatas/static/media/dog-meg.ce017cce.jpg"
+  },
+  {
+    "revision": "e91e5196197fc74b408be940f6ea6d46",
+    "url": "/adotapatas/static/media/dog-woody.e91e5196.JPG"
+  },
+  {
+    "revision": "d91af8a3713a48a7686bce2c97f51e6b",
+    "url": "/adotapatas/static/media/home-page.d91af8a3.png"
+  }
+]);
